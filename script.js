2193f35b389e92c8ad62222b8156284a0eae3c6a
leticea <nascimento.leticeah@gmail.com>
2022-12-31 14:40:22 -0300

    criando função setInterval

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ const min = document.querySelector("#min");
 const sec = document.querySelector("#sec");
 
 setInterval(() => {
+  const date = new Date();
+  const hh = date.getHours() * 30;
+  const mm = date.getMinutes() * degree;
+  const ss = date.getSeconds() * degree;
+
   
 });
