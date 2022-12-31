13c4f7d000f2bd1d6f046f0acf7e5ded0da67453
leticea <nascimento.leticeah@gmail.com>
2022-12-31 14:36:50 -0300

    criando const

---
+++ script.js
@@ -0,0 +1,8 @@
+const degree = 6;
+const hr = document.querySelector("#hr");
+const min = document.querySelector("#min");
+const sec = document.querySelector("#sec");
+
+setInterval(() => {
+  
+});
